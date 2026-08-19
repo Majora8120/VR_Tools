14db60df1522d9822aa07a85f6caa82996bd1176
Majora8120 <91237086+Majora8120@users.noreply.github.com>
2026-08-19 17:21:03 -0500

    Add Rewritten ASW functions

---
+++ VR_Tools/ASW.cs
@@ -0,0 +1,55 @@
+﻿#pragma warning disable CA1416 // Validate platform compatibility
+using System;
+using Microsoft.Win32;
+
+namespace VR_Tools
+{
+	internal class ASW
+	{
+		private const string _keyPath = @"SOFTWARE\Oculus";
+		private const string _valueName = "AswDisabled";
+
+		public static void Enable()
+		{
+			try
+			{
+				RegistryKey? key = Registry.LocalMachine.OpenSubKey(_keyPath);
+				key ??= Registry.LocalMachine.CreateSubKey(_keyPath);
+				key.SetValue(_valueName, 1, RegistryValueKind.DWord);
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.Message);
+			}
+		}
+
+		public static void Disable()
+		{
+			try
+			{
+				RegistryKey? key = Registry.LocalMachine.OpenSubKey(_keyPath);
+				key ??= Registry.LocalMachine.CreateSubKey(_keyPath);
+				key.SetValue(_valueName, 0, RegistryValueKind.DWord);
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.Message);
+			}
+		}
+
+		public static bool IsDisabled()
+		{
+			try
+			{
+				RegistryKey? key = Registry.LocalMachine.OpenSubKey(_keyPath);
+				key ??= Registry.LocalMachine.CreateSubKey(_keyPath);
+				return (bool?)key.GetValue(_valueName) ?? false;
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.Message);
+				return false;
+			}
+		}
+	}
+}
