f82f906b4e7be8ade62a5a303267a764761a5265
Majora8120 <91237086+Majora8120@users.noreply.github.com>
2026-08-19 17:21:34 -0500

    Add log window

---
+++ VR_Tools/Views/LogWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace VR_Tools.Views;
+
+public partial class LogWindow : Window
+{
+    public LogWindow()
+    {
+        InitializeComponent();
+    }
+}
